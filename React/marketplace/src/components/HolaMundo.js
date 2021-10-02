b966d20efa5eb750b1cc100bc5022c01d15fcad7
ccamargo8 <-ingcarloscamargo87@hotmail.com->
2021-10-02 11:07:49 -0500

    Clase React
    Clase react, implemenetación Ant y más

---
+++ React/marketplace/src/components/HolaMundo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function HolaMundo() {
+    
+    return(
+        <div>
+            <h1>Hola, Bienvenido</h1>
+        </div>
+    )
+}
+
+export function Adios(){
+    return(
+        <div>
+            <h1>Adiós!</h1>
+        </div>
+    )
+}
+
