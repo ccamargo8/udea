d40912dbec609cd2fb3eb7e10d10a306957821ba
ccamargo8 <-ingcarloscamargo87@hotmail.com->
2021-10-05 21:37:01 -0500

    Updated info
    Updated info

---
+++ React/marketplace/cliente/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+/* import './App.css'; */
+import HolaMundo, { Adios } from './components/HolaMundo';
+import Saludar from './components/Saludar';
+import "./App.scss";
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <HolaMundo/>
+          <Adios/>
+          <Saludar name="Carlos"/>          
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ React/marketplace/server/models/user.js
@@ -0,0 +1,9 @@
+const user = {
+    name: String,
+    lastname: String,
+    email:{
+        type: String, unique: TextTrackCue
+    },
+    password: String,
+    active: Boolean
+}
