629a33519b08da55595c587571248d1ff9fbdf7f
ccamargo8 <-ingcarloscamargo87@hotmail.com->
2021-11-10 21:48:37 -0500

    TEst
    Test

--- React/marketplace/server/app.js
+++ React/marketplace/server/app.js
@@ -1,9 +1,10 @@
-const express = require ('express');
-const bodyparser = require("bodyparser");
+const express = require ("express");
+const bodyParser = require ("body-parser");
 
-const app = express() //Debemos traer nuestra versión de la API / config.js
-
-const{API_VERSION}=require('./config');
+const app = express(); //Debemos traer nuestra versión de la API | config.js
+const { API_VERSION } = require ('./config');
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+module.exports = app;
--- React/marketplace/server/index.js
+++ React/marketplace/server/index.js
@@ -7,20 +7,21 @@ const port = process.env.PORT || 3977;
 const { API_VERSION, IP_SERVER, PORT_DB } = require ("./config");
 
 
-mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/web_personal`, //Llamar la conexión
+mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/marketplace`, //Llamar la conexión
 
 //Validación de credenciales de conexión.
-{useNewUrlParser: true }, (err, res) =>{
+{useNewUrlParser: true}, (err, res) => {
     if(err){
         throw err;
     }else{
         console.log("La conexión a la base de datos es correcta");
 
-        app.listen(port, () =>{
-            console.log("#########");
-            console.log("###API RESTE###");
-            console.log("#########");
-            console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);
+        app.listen(port,()=>{
+            console.log("############################");
+            console.log("##### API REST #####");
+            console.log("############################");
+            console.log(`http://${IP_SERVER}:${PORT_DB}/api/${API_VERSION}/`);
         })
     }
 });
+
